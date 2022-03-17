37c7dc8bbc928e2f54377c73db22d3dd78a729bf
ZhengJieWen <38306482@qq.com>
2022-03-17 16:50:05 +0800

    修改代码

--- src/main/java/com/pojo/User.java
+++ src/main/java/com/pojo/User.java
@@ -3,4 +3,28 @@ package com.pojo;
 public class User {
     private int id;
     private String userName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
 }
